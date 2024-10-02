47aac2b4eb7fd11ec15ade47d22a1a08e183bfbe
root <root@Xpc>
2024-10-02 13:29:53 -0400

    changes files

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,29 @@
-def add(a,b):
-    return a + b
+from calculator.operations import add, subtract, multiply, divide
 
-def subtract(a,b):
-    return a - b
+class Calculation:
+    def __init__(self, a, b, operation):
+        self.a = a
+        self.b = b
+        self.operation = operation
+
+    def get_result(self):
+
+        return self.operation(self.a, self.b)
+    
+class Calculator: 
+    @staticmethod
+    def add(a,b):
+        calculation = Calculation(a, b, add)
+        return calculation.get_result()
+    @staticmethod
+    def subtract(a,b):
+        calculation = Calculation(a, b, subtract)
+        return calculation.get_result()
+    @staticmethod
+    def multiply (a,b):
+        calculation = Calculation(a, b, multiply)
+        return calculation.get_result()
+    @staticmethod
+    def divide(a,b):
+        calculation = Calculation(a, b, divide)
+        return calculation.get_result()
