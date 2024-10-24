a0f1da6b7a024ff44f8aa378fba1e016a4905ce2
root <root@Xpc>
2024-10-23 23:23:38 -0400

    change files

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,29 @@
+import os
 import pkgutil
 import importlib
+import sys
 from app.commands import CommandHandler
 from app.commands import Command
 class App:
     def __init__(self): # Constructor
         self.command_handler = CommandHandler()
+
+    def configure_logging(self):
+        logging_conf_path = 'logging.conf'
+        if os.path.exists(logging_conf_path):
+            logging.config.fileConfig(logging_conf_path, disable_existing_loggers=False)
+        else:
+            logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.info("Logging configured.")
+
+    def load_environment_variables(self):
+        settings = {key: value for key, value in os.environ.items()}
+        # print(settings)
+        logging.info("Environment variables loaded.")
+        return settings
+
+    def get_environment_variable(self, env_var: str = 'ENVIRONMENT'):
+        return self.settings.get(env_var, None)
 
     def load_plugins(self):
         # Dynamically load all plugins in the plugins directory
